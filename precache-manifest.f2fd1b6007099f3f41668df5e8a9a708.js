aac358a90216cbfd771151756eb45dcdf11f416f
Cindy Son <cindyson@mail.com>
2018-10-28 22:08:07 -0700

    Updates

---
+++ precache-manifest.f2fd1b6007099f3f41668df5e8a9a708.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36e7d5994475bdf598ca",
+    "url": "/yesno/static/js/runtime~main.36e7d599.js"
+  },
+  {
+    "revision": "d76b7a5c1b93994e4cfc",
+    "url": "/yesno/static/js/main.d76b7a5c.chunk.js"
+  },
+  {
+    "revision": "c78411f9b536d6897ad5",
+    "url": "/yesno/static/js/1.c78411f9.chunk.js"
+  },
+  {
+    "revision": "d76b7a5c1b93994e4cfc",
+    "url": "/yesno/static/css/main.afe601d1.chunk.css"
+  },
+  {
+    "revision": "4869ca23be1cc2a99bd6b5ef3617c34d",
+    "url": "/yesno/index.html"
+  }
+];
